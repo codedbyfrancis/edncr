a0c2ae948fed26c0ebbf613f3f16417b49e158e0
codedbyfrancis <codedbyfrancis@gmail.com>
2025-07-27 14:03:43 +0400

    Supabase migration

--- app/pages/dashboard/profile.vue
+++ app/pages/dashboard/profile.vue
@@ -10,6 +10,10 @@ const supabase = useSupabaseClient();
 const { data } = await supabase.auth.getClaims();
 const user = data?.claims;
 
+console.log(data);
+
+const { data: profileData, error } = await supabase.from('profiles').select();
+
 const config = useRuntimeConfig();
 useSeoMeta({
   title: 'Profile',
@@ -28,6 +32,17 @@ useSeoMeta({
     <div class="page">
       <h1>Profile {{ $t('language') }}</h1>
       <h2>{{ $t('welcome') }}</h2>
+      <div>Modified: {{ useTimeAgo(profileData[0].modified_at) }}</div>
+      <div>
+        Modified:
+        {{
+          useDateFormat(
+            profileData[0].modified_at,
+            'dddd, MMMM D, YYYY [at] hh:mm A'
+          )
+        }}
+      </div>
+      <pre>{{ profileData }}</pre>
       <pre>{{ data }}</pre>
     </div>
   </section>

cc03b62a1d552ceb7fba3a18d2deee627570bd85
codedbyfrancis <codedbyfrancis@gmail.com>
2025-07-28 13:33:26 +0400

    Fix HeaderLogo

--- app/components/Default/HeaderLogo.vue
+++ app/components/Default/HeaderLogo.vue
@@ -7,24 +7,23 @@ const config = useRuntimeConfig();
     <div>
       <LogoCbf :fill="`var(--logo-fill)`" :width="2" :height="2" />
     </div>
-    <div>
-      <div
-        class="font-[Inika] hidden md:block text-3xl font-semibold text-[var(--logo-fill)]"
-      >
-        <span class="cbf-logo-text">{{ config.public.appName }}</span>
-      </div>
-      <div
-        class="font-[Inika] flex md:hidden items-center text-4xl font-semibold text-[var(--logo-fill)]"
-      >
-        <span class="cbf-logo-text">{{ config.public.appName }}</span>
-      </div>
+
+    <div
+      class="md:flex-row md:flex items-center font-[Inika] hidden text-3xl font-semibold text-[var(--logo-fill)]"
+    >
+      <span class="cbf-logo-text">{{ config.public.appName }}</span>
+    </div>
+    <div
+      class="md:hidden flex flex-row items-center font-[Inika] text-4xl font-semibold text-[var(--logo-fill)]"
+    >
+      <span class="cbf-logo-text">{{ config.public.appName }}</span>
     </div>
   </div>
 </template>
 <style lang="css" scoped>
 @reference "tailwindcss";
 .cbf-logo-text {
-  @apply font-[Inika] text-3xl md:text-2xl text-inherit;
+  @apply font-[Inika] text-3xl md:text-2xl text-inherit text-nowrap;
 }
 .cbf-logo-by {
   @apply italic text-[var(--logo-fill)]/75 text-3xl;

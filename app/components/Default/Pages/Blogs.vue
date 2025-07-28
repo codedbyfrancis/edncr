fbc8620663acc9aa05ca202ff72eead3ddc9ca8c
codedbyfrancis <codedbyfrancis@gmail.com>
2025-07-28 07:51:15 +0400

    Added pages and blog placeholders

--- app/components/Default/Pages/Blogs.vue
+++ app/components/Default/Pages/Blogs.vue
@@ -1,3 +1,17 @@
+<script lang="ts" setup>
+defineProps({
+  data: { type: Object, default: '' },
+});
+</script>
 <template>
-  <div>/Default/Pages/Blogs.vue</div>
+  <section>
+    <div class="page">
+      <small>/Default/Pages/Blogs.vue</small>
+      <div>
+        <h1>{{ data.title }}</h1>
+        <h2>{{ $t('welcome') }}</h2>
+        <p>{{ data.description }}</p>
+      </div>
+    </div>
+  </section>
 </template>

0156bdf8f1616c9612e8e1ad999b2570a2cebf74
codedbyfrancis <codedbyfrancis@gmail.com>
2025-08-04 16:21:42 +0400

    Clean Animations.vue

--- app/pages/animations.vue
+++ app/pages/animations.vue
@@ -1,21 +1,6 @@
 <script lang="ts" setup>
-import { AnimationsButtons } from '#components';
-
 definePageMeta({
   layout: 'defaultpages',
-});
-
-const props = defineProps<{
-  color?: 'primary' | 'secondary' | string;
-  classOverride?: string;
-}>();
-
-const attrs = useAttrs();
-
-const bgClass = computed(() => {
-  if (props.color === 'secondary') return 'bg-secondary';
-  if (props.color === 'primary') return 'bg-primary';
-  return typeof props.color === 'string' ? props.color : 'bg-primary';
 });
 </script>
 <template>

f8eee1572616a4ced25f949427f7edc4320e1ddd
codedbyfrancis <codedbyfrancis@gmail.com>
2025-08-20 15:47:16 +0400

    Fix branding

--- app/components/Default/HeaderLogo.vue
+++ app/components/Default/HeaderLogo.vue
@@ -13,21 +13,17 @@ import LogoMain from '~/assets/logo/logo.svg';
     <div
       class="md:flex-row md:flex items-center font-[Inika] hidden text-3xl font-semibold text-[var(--logo-fill)]"
     >
-      <span class="cbf-logo-text">{{ config.public.appName }}</span>
+      <!-- Desktop -->
+      <span class="cbf-logo-text">{{ config.public.appName }}1</span>
     </div>
     <div
-      class="md:hidden flex flex-row items-center font-[Inika] text-4xl font-semibold text-[var(--logo-fill)]"
+      class="md:hidden flex flex-row items-center font-[Inika] text-2xl font-semibold text-[var(--logo-fill)]"
     >
-      <span class="cbf-logo-text">{{ config.public.appName }}</span>
+      <!-- Mobile -->
+      <span class="cbf-logo-text">{{ config.public.appName }}2</span>
     </div>
   </div>
 </template>
 <style lang="css" scoped>
 @reference "tailwindcss";
-.cbf-logo-text {
-  @apply font-[Inika] text-3xl md:text-2xl text-inherit text-nowrap;
-}
-.cbf-logo-by {
-  @apply italic text-[var(--logo-fill)]/75 text-3xl;
-}
 </style>

61f5fbea6bff14d3989a41f653a686d7dcda3374
codedbyfrancis <codedbyfrancis@gmail.com>
2025-08-27 12:18:02 +0400

    Fix call card

--- app/components/Default/Cards/CallInfo.vue
+++ app/components/Default/Cards/CallInfo.vue
@@ -3,10 +3,10 @@
     <div
       class="flex flex-col gap-1 p-4 rounded-t-4xl bg-[var(--surface-container)] text-[var(--on-surface)] text-center"
     >
-      <span class="text-sm uppercase">Business Category</span>
+      <span class="text-sm uppercase opacity-70">Business Category</span>
       <span class="text-lg font-medium">Company Name LLC</span>
-      <span class="text-4xl py-2 font-extrabold">056 123 4567</span>
-      <span class="text-base">wants to call you.</span>
+      <span class="text-4xl py-2 font-bold">056 123 4567</span>
+      <span class="text-base opacity-70">wants to call you.</span>
     </div>
     <div
       class="py-2 bg-[var(--surface-container)] text-[var(--on-surface)] text-center"
@@ -14,7 +14,7 @@
       <span>4 Stars Rating, Company is trustworthy</span>
     </div>
     <div
-      class="flex flex-col gap-1 p-4 rounded-b-4xl bg-[var(--surface-container)] text-[var(--on-surface)]"
+      class="flex flex-col gap-1 px-6 py-4 rounded-b-4xl bg-[var(--surface-container)] text-[var(--on-surface)]"
     >
       <span>Call details:</span>
       <ul>

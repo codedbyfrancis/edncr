43731e7f56458e3809fa3661fe79f5243546e09b
codedbyfrancis <codedbyfrancis@gmail.com>
2025-09-02 14:36:27 +0400

    Added campaign upload

--- app/components/Default/DashboardMenu.vue
+++ app/components/Default/DashboardMenu.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>Menu</div>
+  <div>Default Dashboard Menu</div>
+  <div class="flex gap-3">
+    <NuxtLinkLocale :to="{ name: 'dashboard' }">Dashboard</NuxtLinkLocale>
+    <NuxtLinkLocale :to="{ name: 'dashboard-upload-campaign' }"
+      >Upload Campaign</NuxtLinkLocale
+    >
+  </div>
 </template>
---
+++ app/pages/dashboard/upload-campaign.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+  layout: 'defaultdashboard',
+});
+import { CalendarDate } from '@internationalized/date';
+const supabase = useSupabaseClient();
+// const {
+//   data: { claims },
+// } = await supabase.auth.getClaims();
+const { data } = await supabase.auth.getClaims();
+const user = data?.claims;
+
+// const user = useSupabaseUser();
+const config = useRuntimeConfig();
+useSeoMeta({
+  title: 'Dashboard/Upload Campaign',
+  ogTitle: 'Dashboard/Upload Campaign' + ' | ' + config.public.appName,
+  description: 'Dashboard/Upload Campaign description',
+  ogDescription: 'Dashboard/Upload Campaign description for social media',
+  ogUrl: config.public.appUrl + '/dashboard/upload-campaign',
+  ogType: 'website',
+  ogSiteName: config.public.appName,
+  ogImage: config.public.imageCdnUrl + '/icons/android-chrome-512x512.png',
+  twitterCard: 'summary_large_image',
+});
+const name = ref('');
+const details = ref('');
+const target_area = ref('');
+const today = new Date();
+const daterange = ref({
+  start: new CalendarDate(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    today.getDate()
+  ),
+  end: new CalendarDate(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    today.getDate() + 7
+  ),
+});
+</script>
+<template>
+  <div>
+    <h1>Upload Campaign</h1>
+    <div class="flex flex-col w-full lg:w-1/2 mx-auto gap-3">
+      <div class="flex flex-col">
+        <label for="name">Campaign name:</label
+        ><UInput v-model="name" id="name" />
+      </div>
+      <div class="flex flex-col">
+        <label for="details">Campaign details:</label>
+        <UTextarea v-model="details" id="details" />
+      </div>
+      <div class="flex flex-col">
+        <label for="target_area">Campaign target area:</label>
+        <UInput v-model="target_area" id="target_area" />
+      </div>
+      <div class="flex flex-col">
+        <label for="target_area">Campaign duration:</label>
+        <div>
+          <UCalendar
+            range
+            v-model="daterange"
+            :min-value="
+              new CalendarDate(
+                today.getFullYear(),
+                today.getMonth() + 1,
+                today.getDate()
+              )
+            "
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
